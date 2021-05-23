37b0c130541c6e2980a6a9774499249ce160d99b
Michael MacLean <20232905+hanabanashiku@users.noreply.github.com>
2021-05-23 07:43:41 -0400

    Support for IEnumerable, nulls, and JsonSerializerOptions (#1)
    * Added support for rendering IEnumerable types
    
    * Added support for nested objects
    
    * Added support for null values and JsonSerializerOptions
    
    * Code cleanup
    
    * Added extra unit test for nulls
    
    * Removed one file
    
    * Small fix for AddNullProperty
    
    * Added missing test

--- AJP.JsonElementExtensions.UnitTests/JsonElementExtensionsTests.cs
+++ AJP.JsonElementExtensions.UnitTests/JsonElementExtensionsTests.cs
@@ -1,7 +1,6 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text.Json;
 
@@ -14,7 +13,7 @@ namespace AJP.JsonElementExtensions.UnitTests
         public void Various_AddProperty_methods_should_add_properties_that_can_be_asserted_in_the_output()
         {
 			// get a JsonElement to start with...
-			var jsonString = "{ \"Name\": \"Andrew\", \"EmailAddress\": \"a@b.com\" }";
+			const string jsonString = "{ \"Name\": \"Andrew\", \"EmailAddress\": \"a@b.com\" }";
 			var jElement = JsonDocument.Parse(jsonString).RootElement;
 
 			jElement = jElement
@@ -22,13 +21,19 @@ namespace AJP.JsonElementExtensions.UnitTests
 				.AddProperty("Male", true)
 				.AddProperty("Female", false)
 				.AddNullProperty("Alien")
-				.AddProperty("Roles", new string[] { "admin", "user" })
+				.AddProperty("Roles", new[] {"admin", "user"})
+				.AddProperty("Foods", new List<string> {"mac and cheese", "lasagna"})
 				.AddProperty("LastUpdated", new DateTime(2020, 2, 27, 22, 09, 00))
-				.AddProperty("crazyNewObject", new
-					{
-						Name = "Hobbies",
-						Value = "bass guitar and writing c# code"
-					});
+				.AddProperty("crazyNewObject", new {
+					Name = "Hobbies",
+					Value = "bass guitar and writing c# code"
+				})
+				.AddProperty("nestedObject", new {
+					Object = new {
+						Name = "Hobbies"
+					}
+				})
+				.AddProperty("nullProperty", null);
 
 			Assert.That(jElement.GetProperty("Age").ToString(), Is.EqualTo("38"));
 			Assert.That(jElement.GetProperty("Male").GetBoolean(), Is.EqualTo(true));
@@ -36,15 +41,60 @@ namespace AJP.JsonElementExtensions.UnitTests
 			Assert.That(jElement.GetProperty("Alien").GetString(), Is.EqualTo(null));
 			Assert.That(jElement.GetProperty("Roles").GetArrayLength(), Is.EqualTo(2));
 			Assert.That(jElement.GetProperty("Roles").EnumerateArray().FirstOrDefault().GetString(), Is.EqualTo("admin"));
+			Assert.That(jElement.GetProperty("Foods").GetArrayLength(), Is.EqualTo(2));
+			Assert.That(jElement.GetProperty("Foods").EnumerateArray().Select(x => x.GetString()).ToArray(), Is.EqualTo(new[] {"mac and cheese", "lasagna"}));
 			Assert.That(jElement.GetProperty("LastUpdated").GetString(), Is.EqualTo(new DateTime(2020, 2, 27, 22, 09, 00).ToString("s")));
 			Assert.That(jElement.GetProperty("crazyNewObject").EnumerateObject().FirstOrDefault().Value.ToString(), Is.EqualTo("Hobbies"));
+			Assert.That(jElement.GetProperty("nestedObject").GetProperty("Object").GetProperty("Name").GetString(), Is.EqualTo("Hobbies"));
+			Assert.That(jElement.GetProperty("nullProperty").ValueKind, Is.EqualTo(JsonValueKind.Null));
+        }
+
+        [Test]
+        public void AddProperty_method_respects_json_options() 
+        {
+	        var options = new JsonSerializerOptions {
+		        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		        IgnoreNullValues = true
+	        };
+		        
+	        // get a JsonElement to start with...
+	        const string jsonString = "{ \"Name\": \"Andrew\", \"EmailAddress\": \"a@b.com\" }";
+	        var jElement = JsonDocument.Parse(jsonString).RootElement;
+
+	        var obj = new {
+		        Hello = "hello",
+		        World = (string)null
+	        };
+
+	        jElement = jElement
+		        .AddProperty("withOptions", obj, options)
+		        .AddProperty("withoutOptions", obj)
+		        .AddNullProperty("NullWithOptions", options)
+		        .AddNullProperty("NullWithoutOptions")
+		        .AddNullProperty("NullWithoutIgnoreNull",
+			        new JsonSerializerOptions {PropertyNamingPolicy = JsonNamingPolicy.CamelCase});
+
+	        var withOptions = jElement.GetProperty("withOptions");
+	        var withoutOptions = jElement.GetProperty("withoutOptions"); 
+	        
+	        Assert.That(withOptions.TryGetProperty("hello", out var _), Is.True);
+	        Assert.That(withOptions.TryGetProperty("Hello", out var _), Is.Not.True);
+	        Assert.That(withOptions.TryGetProperty("world", out var _), Is.Not.True);
+	       
+	        Assert.That(withoutOptions.TryGetProperty("Hello", out var _), Is.True);
+	        Assert.That(withoutOptions.TryGetProperty("hello", out var _), Is.Not.True);
+	        Assert.That(withoutOptions.TryGetProperty("World", out var _), Is.True);
+
+	        Assert.That(jElement.TryGetProperty("nullWithOptions", out var _), Is.Not.True);
+	        Assert.That(jElement.TryGetProperty("NullWithoutOptions", out var _), Is.True);
+	        Assert.That(jElement.TryGetProperty("nullWithoutIgnoreNull", out var _), Is.True);
         }
 
 		[Test]
 		public void ParseAsJsonStringAndMutate_method_should_add_properties_that_can_be_asserted_in_the_output()
 		{
 			// get a JsonElement to start with...
-			var jsonString = "{ \"Name\": \"Andrew\", \"EmailAddress\": \"a@b.com\" }";
+			const string jsonString = "{ \"Name\": \"Andrew\", \"EmailAddress\": \"a@b.com\" }";
 			var jElement = JsonDocument.Parse(jsonString).RootElement;
 
 			jElement = jElement.ParseAsJsonStringAndMutate((utf8JsonWriter1, namesOfPropertiesToRemove) => 
@@ -61,7 +111,7 @@ namespace AJP.JsonElementExtensions.UnitTests
 		public void RemoveProperty_methods_should_remove_properties_from_the_output()
 		{
 			// get a JsonElement to start with...
-			var jsonString = "{ \"Name\": \"Andrew\", \"EmailAddress\": \"a@b.com\" }";
+			const string jsonString = "{ \"Name\": \"Andrew\", \"EmailAddress\": \"a@b.com\" }";
 			var jElement = JsonDocument.Parse(jsonString).RootElement;
 
 			jElement = jElement
@@ -74,7 +124,7 @@ namespace AJP.JsonElementExtensions.UnitTests
 		public void RemoveProperties_methods_should_remove_properties_from_the_output()
 		{
 			// get a JsonElement to start with...
-			var jsonString = "{ \"Name\": \"Andrew\", \"EmailAddress\": \"a@b.com\", \"Age\": 38 }";
+			const string jsonString = "{ \"Name\": \"Andrew\", \"EmailAddress\": \"a@b.com\", \"Age\": 38 }";
 			var jElement = JsonDocument.Parse(jsonString).RootElement;
 
 			jElement = jElement.RemoveProperties(new List<string> { "EmailAddress", "Age" });
@@ -86,7 +136,7 @@ namespace AJP.JsonElementExtensions.UnitTests
         [Test] public void AddProperty_method_should_handle_primitive_style_objects_cast_as_objects()
         {
             // get a JsonElement to start with...
-            var jsonString = "{ \"Name\": \"Andrew\", \"EmailAddress\": \"a@b.com\", \"Age\": 38 }";
+            const string jsonString = "{ \"Name\": \"Andrew\", \"EmailAddress\": \"a@b.com\", \"Age\": 38 }";
             var jElement = JsonDocument.Parse(jsonString).RootElement;
 
             var stringCastToObject = (object) "3jk4h5gkj3hg45kjh4g5";
@@ -99,7 +149,7 @@ namespace AJP.JsonElementExtensions.UnitTests
         [Test] public void JsonElement_ConvertToObject_method_should_return_the_specified_object()
         {
 	        // get a JsonElement to start with...
-	        var jsonString = "{ \"Name\": \"Andrew\", \"EmailAddress\": \"a@b.com\", \"Age\": 38 }";
+	        const string jsonString = "{ \"Name\": \"Andrew\", \"EmailAddress\": \"a@b.com\", \"Age\": 38 }";
 	        var jElement = JsonDocument.Parse(jsonString).RootElement;
 
 	        var result = jElement.ConvertToObject<TestClass>();
@@ -111,7 +161,7 @@ namespace AJP.JsonElementExtensions.UnitTests
         [Test] public void JsonDocument_ConvertToObject_method_should_return_the_specified_object()
         {
 	        // get a JsonElement to start with...
-	        var jsonString = "{ \"Name\": \"Andrew\", \"EmailAddress\": \"a@b.com\", \"Age\": 38 }";
+	        const string jsonString = "{ \"Name\": \"Andrew\", \"EmailAddress\": \"a@b.com\", \"Age\": 38 }";
 	        var jDoc = JsonDocument.Parse(jsonString);
 	        
 	        var result = jDoc.ConvertToObject<TestClass>();
